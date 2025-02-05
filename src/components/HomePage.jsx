6604e84c07fa709303bc3475cc4177afbd7adc1f
Mohammed-Atheeq <m.atheeq15196@gmail.com>
2025-02-05 20:31:26 +0530

    Deployed to Netlify

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -196,7 +196,7 @@ function HomePage() {
 
           <div className="resume">
             <a
-              href="/portfolio/resume.pdf"
+              href="/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
